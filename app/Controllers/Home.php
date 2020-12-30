9bf3a6d28a181ac2a53bb00111d03138eb90560d
timotiusvincent26 <timotiusvincent26@gmail.com>
2020-12-30 17:10:25 +0700

    memperbaiki icon pada footer, menambahkan view dokumentasi (masih pada controller home)

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('layout/template.php');
+		return view('dokumentasi.php');
 	}
 
 	//--------------------------------------------------------------------
