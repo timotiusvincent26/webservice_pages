411b2a4575ae3ccadfd5688f493deef9edebe6e2
aldihamidi <71486363+aldihamidi@users.noreply.github.com>
2021-01-23 21:38:21 +0700

    add daftar.php

---
+++ app/Views/Login/daftar.php
@@ -0,0 +1,10 @@
+<?= $this->extend('Login/templateLogin.php'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="rounded-xl shadow-2xl pl-8 w-full pt-2 xl:mx-12 lg:mx-0 sm:mx-6 mx-2">
+    <h3 class="font-bold text-primary text-lg">Panduan Pendaftaran Sistem Informasi Alumni</h3>
+    <a href="/login/" class="text-secondary text-sm font-medium">Kembali ke laman login</a>
+</div>
+
+<?= $this->endSection(); ?>
