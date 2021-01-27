d2a1e1e2c52797e97b0e72f415982a6599dbcd68
timotiusvincent26 <timotiusvincent26@gmail.com>
2021-01-27 18:06:43 +0700

    mengedit tampilan daftar hasil pencarian alumni dan berita berdasarkan UI terbaru

--- app/Views/websia/kontenWebsia/searchAndFilter/searchAndFilter.php
+++ app/Views/websia/kontenWebsia/searchAndFilter/searchAndFilter.php
@@ -114,81 +114,93 @@
 <!-- END FILTER -->
 
 <!-- HASIL PENCARIAN ALUMNI -->
-<div class="bg-primary w-full lg:px-20 md:px-8 px-2 my-8 py-4">
+<div class="ml-12 mr-24 mt-6">
+    <div>
+        <h1 class="text-secondary font-heading text-2xl font-bold">ALUMNI</h1>
+        <!-- awal jumlah hasil pencarian alumni  -->
+        <div class="text-primary mb-6 font-paragraph font-extralight text-sm">
+            Sekitar 2.547 hasil pencarian alumni
+        </div>
+        <hr class="my-4 border-2 border-gray-400">
+        <!-- akhir jumlah hasil pencarian alumni  -->
 
-    <!-- awal jumlah hasil pencarian alumni  -->
-    <div class="text-white mb-6 font-paragraph font-extralightt text-sm text-center">
-        Sekitar 28.899 hasil pencarian alumni
-    </div>
-    <!-- akhir jumlah hasil pencarian alumni  -->
-
-    <div class="w-full grid md:grid-cols-3 grid-cols-2 gap-x-2 gap-y-4">
-        <!-- awal card hasil pencarian alumni -->
-        <?php for ($x = 0; $x < 9; $x++) : ?>
-            <div class="flex justify-center">
-                <div class="rounded-xl bg-white px-4 py-3 flex md:flex-row flex-col justify-between md:gap-x-2">
-                    <div class="flex items-center">
-                        <img src="/img/avatar.png" class="lg:w-20 w-16 mx-auto" alt="">
-                    </div>
-                    <div class="flex items-center">
-                        <div>
-                            <h2 class="font-heading text-primary lg:text-lg md:text-base text-sm font-heading font-semibold">Nama Lengkap</h2>
-                            <h3 class="font-paragraph text-primary md:text-sm text-xs font-paragraph">Atribut</h3>
+        <!-- DAFTAR HASIL PENCARIAN ALUMNI -->
+        <div>
+            <?php for ($x = 0; $x < 4; $x++) : ?>
+                <div class="mx-2">
+                    <div class="flex gap-x-4">
+                        <div class="flex items-center">
+                            <img src="/img/avatar.png" class="lg:w-18 w-16 mx-auto" alt="">
+                        </div>
+                        <div class="flex items-center">
+                            <div>
+                                <h2 class="text-lg font-heading text-primary font-semibold">Nama Lengkap</h2>
+                                <div class="text-sm font-paragraph text-primary">Atribut</div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        <?php endfor; ?>
-        <!-- akhir card hasil pencarian alumni -->
-
-    </div>
-
-    <!-- awal tulisan "Selengkapnya" di hasil pencarian -->
-    <div class="flex justify-end mt-6">
-        <div class="flex bg-secondary text-white rounded-full md:py-2 py-1 md:px-3 px-2 items-center gap-x-2 cursor-pointer md:text-sm text-xs">
-            Selengkapnya
-            <img src="/img/right-off.png" class="md:w-4 md:h-4 w-3 h-3" alt="">
+                <hr class="my-4 border-gray-400">
+            <?php endfor; ?>
+            <hr class="-my-4 border-2 border-gray-400">
         </div>
+        <!-- awal tulisan "Selengkapnya" di hasil pencarian -->
+        <div class="flex justify-end mt-12">
+            <div class="flex bg-secondary text-white rounded-full md:py-2 py-1 md:px-3 px-2 items-center gap-x-2 cursor-pointer md:text-sm text-xs">
+                Selengkapnya
+                <img src="/img/right-off.png" class="md:w-4 md:h-4 w-3 h-3" alt="">
+            </div>
+        </div>
+        <!-- akhir tulisan "Selengkapnya" di hasil pencarian -->
+        <!-- END DAFTAR HASIL PENCARIAN ALUMNI -->
     </div>
-    <!-- akhir tulisan "Selengkapnya" di hasil pencarian -->
 </div>
-
 <!-- AKHIR HASIL PENCARIAN ALUMNI -->
 
 <!-- HASIL PENCARIAN BERITA -->
-<div class="w-full lg:px-20 md:px-8 px-2 my-8 py-4">
-    <div class="text-primary mb-2 font-paragraph font-extralightt text-sm text-center">
-        Sekitar 28.899 hasil pencarian alumni
-    </div>
-    <hr class="my-2 border-2 border-gray-400">
-    <!-- DAFTAR HASIL PENCARIAN BERITA -->
+<div class="ml-12 mr-24 mt-6">
     <div>
-
-        <!-- awal card hasil pencarian berita  -->
-        <?php for ($x = 0; $x < 3; $x++) : ?>
-            <div class="mx-4">
-                <h2 class="font-heading text-primary text-lg font-semibold">Judul Berita</h2>
-                <h4 class="font-paragraph text-primary text-xs">11 Januari 2021</h4>
-                <div class="text-justify font-paragraph">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit turpis sem, eu laoreet odio pretium ac. Mauris eget aliquet lorem. Cras dignissim leo non ante molestie, at vulputate justo lobortis. Pellentesque quam elit, mattis eu nibh et, maximus congue mauris
-                </div>
-            </div>
-            <hr class="my-2 border-2 border-gray-400">
-        <?php endfor; ?>
-        <!-- akhir card hasil pencarian berita  -->
-
-    </div>
-    <!-- END DAFTAR HASIL PENCARIAN BERITA -->
-
-    <!-- awal tulisan "Selengkapnya" di hasil pencarian -->
-    <div class="flex justify-end mt-6">
-        <div class="flex bg-secondary text-white rounded-full md:py-2 py-1 md:px-3 px-2 items-center gap-x-2 cursor-pointer md:text-sm text-xs">
-            Selengkapnya
-            <img src="/img/right-off.png" class="md:w-4 md:h-4 w-3 h-3" alt="">
+        <h1 class="text-secondary font-heading text-2xl font-bold">BERITA</h1>
+        <!-- awal jumlah hasil pencarian berita  -->
+        <div class="text-primary mb-6 font-paragraph font-extralight text-sm">
+            Sekitar 28.899 hasil pencarian berita
         </div>
-    </div>
-    <!-- akhir tulisan "Selengkapnya" di hasil pencarian -->
+        <hr class="my-4 border-2 border-gray-400">
+        <!-- akhir jumlah hasil pencarian berita  -->
 
+        <!-- DAFTAR HASIL PENCARIAN BERITA -->
+        <div>
+            <?php for ($x = 0; $x < 4; $x++) : ?>
+                <div class="mx-2">
+                    <div class="flex gap-x-4">
+                        <div class="flex items-center">
+                            <div class="bg-gray-300 w-48 h-28"></div>
+                        </div>
+                        <div class="flex items-center">
+                            <div>
+                                <h2 class="text-lg font-heading text-primary font-semibold mb-2">Judul Berita</h2>
+                                <div class="text-xs font-paragraph text-primary">11 Januari 2021</div>
+                                <div class="text-sm font-paragraph text-primary">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit turpis sem, eu laoreet odio pretium ac. Mauris eget aliquet lorem. Cras dignissim leo non ante molestie, at vulputate justo lobortis. Pellentesque quam elit, mattis eu nibh et, maximus congue mauris
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="my-4 border-gray-400">
+            <?php endfor; ?>
+            <hr class="-my-4 border-2 border-gray-400">
+        </div>
+        <!-- awal tulisan "Selengkapnya" di hasil pencarian -->
+        <div class="flex justify-end mt-12">
+            <div class="flex bg-secondary text-white rounded-full md:py-2 py-1 md:px-3 px-2 items-center gap-x-2 cursor-pointer md:text-sm text-xs">
+                Selengkapnya
+                <img src="/img/right-off.png" class="md:w-4 md:h-4 w-3 h-3" alt="">
+            </div>
+        </div>
+        <!-- akhir tulisan "Selengkapnya" di hasil pencarian -->
+        <!-- END DAFTAR HASIL PENCARIAN BERITA -->
+    </div>
 </div>
 <!-- END HASIL PENCARIAN BERITA -->
 
