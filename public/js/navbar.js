36b33ca1181865ae97c7ba10e320efae5efe6c50
aldihamidi <71486363+aldihamidi@users.noreply.github.com>
2021-02-04 23:54:49 +0700

    add js buat hamburger edit profil

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,8 +1,8 @@
 // awal js buat scroll navbar interaktif
 var lastScroll = 0;
-$(window).scroll(function(event){
+$(window).scroll(function (event) {
     var st = $(this).scrollTop();
-    if (st > lastScroll){
+    if (st > lastScroll) {
         $('.navbar').addClass('invisible');
     } else {
         $('.navbar').removeClass('invisible');
@@ -12,13 +12,13 @@ $(window).scroll(function(event){
 // akhir js buat scroll navbar interaktif
 
 // doropdown navbar mobile
-$('#hamburger').click(function() {
-    if($('#menu').hasClass('hidden')){
+$('#hamburger').click(function () {
+    if ($('#menu').hasClass('hidden')) {
         $('#menu').removeClass('hidden');
     } else {
         $('#menu').addClass('hidden');
     }
-  });
+});
 
 $('#hamburgerApi').click(function () {
     if ($('#menuApi').hasClass('hidden')) {
@@ -28,15 +28,15 @@ $('#hamburgerApi').click(function () {
     }
 });
 
-  $('#inputCari').click(function(){
+$('#inputCari').click(function () {
     $('#tombolCari').addClass('hidden');
-  })
+})
 
 // doropdown navbar mobile
 
 // awal dropdown galeri mobile
-$('#galeri').click(function() {
-    if($('#listGaleri').hasClass('hidden')){
+$('#galeri').click(function () {
+    if ($('#listGaleri').hasClass('hidden')) {
         $('#upGaleri').removeClass('hidden');
         $('#downGaleri').addClass('hidden');
         $('#listGaleri').removeClass('hidden');
@@ -45,5 +45,24 @@ $('#galeri').click(function() {
         $('#downGaleri').removeClass('hidden');
         $('#listGaleri').addClass('hidden');
     }
-  });
+});
 // akhir dropdown galeri mobile
+$('#hamburgerEditProfil').click(function () {
+    if ($('#navEdit').hasClass('hidden')) {
+        $('#navEdit').removeClass('hidden')
+        $('#navEdit').parent().addClass('w-52')
+        $('#hamburgerEditProfil').html(`
+            <svg class="text-white sm:w-8 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+        `)
+    } else {
+        $('#navEdit').addClass('hidden')
+        $('#navEdit').parent().removeClass('w-52')
+        $('#hamburgerEditProfil').html(`
+        <svg class="sm:w-8 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+        `)
+    }
+});
