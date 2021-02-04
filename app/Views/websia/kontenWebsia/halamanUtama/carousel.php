8ac83cfd6527c3441401ad853688bc8d84f9b594
yenitamelia <yenitamelia2@gmail.com>
2021-02-03 17:04:51 -0800

    mengedit sedikit halaman profil dibagian bio

--- app/Views/websia/kontenWebsia/halamanUtama/carousel.php
+++ app/Views/websia/kontenWebsia/halamanUtama/carousel.php
@@ -5,22 +5,22 @@
 <div class="w-full bg-primary">
     <div id="slider" class="swiper-container w-full">
         <div class="swiper-wrapper">
-            <div class="swiper-slide bg-cover bg-center w-full" style="background-image: url('/img/komponen 1.png');">
-                <div class="container py-12 md:20 lg:py-28">
-                    <div class="w-full lg:w-2/3">
-                        <div class="md:py-10 px-14 lg:px-28 md:px-20">
-                            <h2 class="font-heading font-bold text-2xl text-white md:text-6xl">Jalin Silaturahmi Kembali</h2>
-                            <p class="font-paragraph text-xl md:mt-3 text-white font-light">Masuk untuk menjelajah lebih dalam</p>
-                            <hr class="bg-secondary w-20 md:w-28 mt-2 md:mt-4">
+            <div class="swiper-slide bg-cover bg-center w-full" style="background-image: url('/img/carousel1.png');">
+                <div class="container md:py-12 lg:py-28">
+                    <div class="w-full lg:w-4/5">
+                        <div class="lg:py-4 px-14 lg:px-28 md:px-20">
+                            <h2 class="font-heading text-2xl text-white  lg:text-3xl md:ml-0 lg:ml-24 mt-16">Sukseskan pemutakhiran data alumni <br /> <b>AIS / STIS / Politeknik Statistika STIS </b><br> dengan</h2>
+                            <p class="font-heading font-bold text-2xl text-primary  lg:text-3xl md:ml-0 lg:ml-24">Update data diri Anda di <br /> Sistem Informasi Alumni (SIA)</p>
+                            <!-- <hr class="bg-secondary w-20 md:w-28 mt-2 md:mt-4"> -->
                         </div>
-                        <div class="my-8 px-14 lg:px-28 md:px-20 mt-4 md:-mt-2">
-                            <!-- tombol yang ada di dalam carousel masih belum ditentukan ada atau tidaknya, konten masih belum fix -->
-                            <a href="#" class="bg-secondary ease-linear font-paragraph text-xs md:text-xl hover:bg-secondaryhover text-white px-4 py-2 md:px-6 md:py-2 rounded-full">Masuk</a>
-                        </div>
+                        <!-- <div class="my-8 px-14 lg:px-28 md:px-20 mt-4 md:-mt-2">
+                            tombol yang ada di dalam carousel masih belum ditentukan ada atau tidaknya, konten masih belum fix
+                        <a href="#" class="bg-secondary ease-linear font-paragraph text-xs md:text-xl hover:bg-secondaryhover text-white px-4 py-2 md:px-6 md:py-2 rounded-full">Masuk</a>
+                    </div> -->
                     </div>
                 </div>
             </div>
-            <div class="swiper-slide bg-cover bg-center" style="background-image: url('/img/komponen 1.png');">
+            <div class="swiper-slide bg-cover bg-center" style="background-image: url('/img/carousel3.png');">
                 <div class="container py-12 md:20 lg:py-28">
                     <div class="w-full lg:w-2/3">
                         <div class="md:py-10 px-14 lg:px-28 md:px-20">
