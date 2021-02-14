637d3b6db7bc53bb4106f51126e009fdff8b9b30
timotiusvincent26 <timotiusvincent26@gmail.com>
2021-02-15 01:48:11 +0700

    revisi edit akun

--- app/Views/websia/kontenWebsia/editProfile/editAkun.php
+++ app/Views/websia/kontenWebsia/editProfile/editAkun.php
@@ -8,15 +8,15 @@
                 <label for="username" class="font-medium">Username:</label>
                 <input type="text" name="username" id="username" class="inputForm" placeholder="Username">
                 <label for="passbaru" class="font-medium">Password Baru:</label>
-                <input type="password" name="passbaru" id="passbaru" class="inputForm" placeholder="●●●●●●●●">
+                <input type="password" name="passbaru" id="passbaru" class="inputForm" placeholder="🞄🞄🞄🞄🞄🞄🞄🞄">
                 <label for="ulangpassbaru" class="font-medium">Ketik Ulang Password Baru:</label>
-                <input type="password" name="ulangpassbaru" id="ulangpassbaru" class="inputForm" placeholder="●●●●●●●●">
-                <div class="text-red-600 text-xs mb-2 text-justify">Silahkan Masukkan Password Lama Anda untuk Verifikasi!</div>
+                <input type="password" name="ulangpassbaru" id="ulangpassbaru" class="inputForm" placeholder="🞄🞄🞄🞄🞄🞄🞄🞄">
+                <div class="text-secondary text-xs mt-6 mb-2 text-justify">Silakan Masukkan Password Lama Anda untuk Verifikasi!</div>
                 <label for="passlama" class="font-medium">Password Lama:</label>
-                <input type="password" name="passlama" id="passlama" class="inputForm" placeholder="●●●●●●●●">
+                <input type="password" name="passlama" id="passlama" class="inputForm" placeholder="🞄🞄🞄🞄🞄🞄🞄🞄">
             </div>
             <div class="flex justify-end md:mb-6 mt-48">
-                <input type="submit" value="SIMPAN" class="w-24 text-center py-1 bg-secondary hover:bg-secondaryhover text-white rounded-full cursor-pointer" id="submitAkun">
+                <input type="submit" value="SIMPAN" class="w-24 text-center py-1 bg-secondary hover:bg-secondaryhover text-white rounded-full cursor-pointer focus:outline-none" id="submitAkun">
             </div>
         </form>
     </div>
