957fbcd3e41c4d0af3cf55687f3c378516145409
aldihamidi <aldi1hamidi2lbs3@gmail.com>
2021-03-16 06:46:43 +0700

    add sidebar dokumentasi dan contentnya

--- app/Views/webservice/kontenWebservice/dokumentasi/dokumentasi.php
+++ app/Views/webservice/kontenWebservice/dokumentasi/dokumentasi.php
@@ -1,72 +1,55 @@
-<?= $this->extend('webservice/layoutWebservice/templateWebserviceLogin.php'); ?>
+<?= $this->extend('webservice/kontenWebservice/dokumentasi/layoutDokumentasi.php'); ?>
 
-<?= $this->section('content'); ?>
+<?= $this->section('dokumentasi'); ?>
 
-<div class="md:grid md:grid-cols-3 lg:gap-x-16 md:gap-x-8 my-4 md:mx-8 mx-4">
-    <div class="md:col-span-1 mt-8">
-        <h1 class="text-2xl text-center font-heading text-secondary font-bold md:hidden block mb-4">DOKUMENTASI</h1>
-        <!-- SEARCH DOKUMENTASI -->
-        <div class="w-full flex items-center">
-            <input type="text" name="pencarian" id="pencarian" placeholder="Pencarian..." class="border-2 border-gray-300 rounded-lg md:px-3 px-1 md:py-2 py-1 w-full">
-            <button>
-                <div>
-                    <svg class="text-gray-500 hover:text-black ml-3 w-8 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                    </svg>
-                </div>
-            </button>
-        </div>
-        <!-- END SEARCH DOKUMENTASI -->
-        <hr class="border-gray-300 mt-2">
-
-        <!-- MINIMAP UNTUK DOKUMENTASI -->
-        <ul class="mt-4 font-heading text-secondary lg:text-2xl md:text-xl text-lg">
-            <li class="mt-2 font-bold"><a href="#judul1">Judul 1</a>
-                <ul class="ml-4 lg:mt-2 text-black lg:text-xl md:text-lg text-base font-paragraph font-bold">
-                    <li><a href="#sub11">Subjudul 1.1</a></li>
-                    <li><a href="#sub12">Subjudul 1.2</a></li>
-                </ul>
-            </li>
-            <li class="mt-2 font-bold"><a href="#judul2">Judul 2</a>
-                <ul class="ml-4 lg:mt-2 text-black lg:text-xl md:text-lg text-base font-paragraph font-bold">
-                    <li><a href="#sub21">Subjudul 2.1</a></li>
-                    <li><a href="#sub22">Subjudul 2.2</a></li>
-                </ul>
-            </li>
-        </ul>
-        <hr class="border-gray-300 mt-2 md:hidden block">
-        <!-- END MINIMAP UNTUK DOKUMENTASI -->
+<div>
+    <div class="lg:hidden absolute mt-3" id="burgerDok">
+        <svg class="block w-7 cursor-pointer fill-current text-secondary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
     </div>
-    <div class="md:col-span-2 mt-8">
-        <h1 class="lg:text-6xl md:text-3xl text-2xl text-center font-heading text-secondary font-bold md:block hidden">DOKUMENTASI</h1>
+    <section>
+        <div class="text-secondary lg:mx-4 mx-3 ml-10">
+            <h1 class="lg:text-xl">Sistem Informasi Alumni</h1>
+            <h2 class="font-bold lg:text-xl">DOKUMENTASI WEB SERVICE</h2>
+        </div>
+        <hr class="border border-gray-500 transform translate-y-2">
+        <div class="lg:mx-4 mx-3 mt-4 mb-8">
+            <h3 class="text-secondary mb-5">Penjelasan API Sistem Informasi Alumni</h3>
+            <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, consequuntur hic? Nostrum ratione perferendis repellendus aspernatur illum at nam ex asperiores natus excepturi. Quae, nam laboriosam. Odit consequatur repellendus illo neque rerum ut vel ad quidem! Delectus, sed in similique veniam vero, ducimus qui quasi quos ipsa a consequuntur! Possimus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit tempora laboriosam deleniti rerum veniam officia provident quis, aspernatur ad doloribus nihil. Molestias placeat error, architecto dolorem et pariatur iusto. Veniam?</p>
+        </div>
+    </section>
 
-        <!-- DAFTAR DOKUMENTASI -->
-        <h2 class="lg:text-2xl md:text-xl text-lg text-secondary mt-4 font-heading font-bold" id="judul1">Judul 1</h2>
-        <div class="text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    <section>
+        <div class="text-secondary lg:mx-4 mx-3">
+            <h1 class="font-bold lg:text-xl">MEMINTA DATA</h1>
         </div>
-        <h3 class="lg:text-xl md:text-lg mt-4 font-bold" id="sub11">Subjudul 1.1</h3>
-        <div class="text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum was introduced to the digital world in the mid-1980s when Aldus employed it in graphic and word-processing templates for its desktop publishing program PageMaker. Lorem ipsum was introduced to the digital world in the mid-1980s when Aldus employed it in graphic and word-processing templates for its desktop publishing program PageMaker.
+        <hr class="border border-gray-500 transform translate-y-2">
+        <div class="lg:mx-4 mx-3 mt-4 mb-8 text-justify">
+            <p class="">Cara meminta data : </p>
+            <ol class="list-decimal list-inside">
+                <li>Klik menu API kemudian pilih sub menu proyek</li>
+                <li>Klik tombol "Buat Proyek" untuk mendaftarkan proyek baru</li>
+                <li>Masukkan identitas proyek/aplikasi seperti nama, deskripsi proyek, pilih cakupan data yang diiginkan dan klik "buat"</li>
+                <li>Tunggu hingga status permintaan berubah dan Anda akan mendapat kunci beserta token API SIA</li>
+            </ol>
+            <p class="mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus consectetur vel sint ad reprehenderit officia officiis quod expedita magni accusamus eveniet ipsum possimus soluta ipsa non similique et sed quo, deleniti iusto vero necessitatibus ipsam! Excepturi placeat possimus pariatur odit. Consequatur necessitatibus harum, blanditiis aut explicabo vitae repudiandae in iusto?</p>
         </div>
-        <h3 class="lg:text-xl md:text-lg mt-4 font-bold" id="sub12">Subjudul 1.2</h3>
-        <div class="text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </div>
-        <h2 class="lg:text-2xl md:text-xl text-lg text-secondary mt-4 font-heading font-bold" id="judul2">Judul 2</h2>
-        <div class="text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-        </div>
-        <h3 class="lg:text-xl md:text-lg mt-4 font-bold" id="sub21">Subjudul 2.1</h3>
-        <div class="text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum was introduced to the digital world in the mid-1980s when Aldus employed it in graphic and word-processing templates for its desktop publishing program PageMaker. Lorem ipsum was introduced to the digital world in the mid-1980s when Aldus employed it in graphic and word-processing templates for its desktop publishing program PageMaker.
-        </div>
-        <h3 class="lg:text-xl md:text-lg mt-4 font-bold" id="sub22">Subjudul 2.2</h3>
-        <div class="text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </div>
-        <!-- END DAFTAR DOKUMENTASI -->
-    </div>
+    </section>
 </div>
+
+<script>
+    $('#burgerDok').click(function() {
+        $('#sidebarDok').removeClass('w-0').addClass('sm:w-1/2 w-3/4')
+        setTimeout(() => {
+            $('#sidebarDok').children().eq(1).removeClass('-left-72').addClass('left-0')
+        }, 0);
+    })
+
+    $('#sidebarDok').children().first().children().first().click(function() {
+        $('#sidebarDok').removeClass('sm:w-1/2 w-3/4').addClass('w-0')
+        $('#sidebarDok').children().eq(1).removeClass('left-0').addClass('-left-72')
+    })
+</script>
 
 <?= $this->endSection(); ?>
---
+++ app/Views/webservice/kontenWebservice/dokumentasi/layoutDokumentasi.php
@@ -0,0 +1,158 @@
+<?= $this->extend('webservice/layoutWebservice/templateWebserviceLogin.php'); ?>
+
+<?= $this->section('content'); ?>
+
+<style>
+    #sidebarDok {
+        z-index: 5;
+    }
+
+    @media (min-width: 1024px) {
+        #sidebarDok {
+            width: 1500px;
+        }
+    }
+
+    #menu1:hover,
+    #menu1+ul>li>div:hover,
+    #menu1+ul>li>ul>li:hover {
+        background-color: rgb(1, 89, 152);
+    }
+
+    #menuDok>li:first-child:hover,
+    #menuDok>li>div:hover,
+    #menuDok>li>ul>li:hover {
+        background-color: rgb(1, 89, 152);
+    }
+
+    .open-submenu {
+        max-height: 65px;
+    }
+
+    .close-submenu {
+        max-height: 0;
+    }
+</style>
+<script src="https://code.jquery.com/jquery-1.10.1.min.js" integrity="sha256-SDf34fFWX/ZnUozXXEH0AeB+Ip3hvRsjLwp6QNTEb3k=" crossorigin="anonymous"></script>
+
+<div class="w-full">
+    <div class="flex w-full h-full relative">
+        <div id="sidebarDok" class="lg:relative absolute top-0 bottom-0 w-0 transition-all duration-1000 easy-out" style="background-color: rgb(4,72,120)">
+            <!-- start sidebar hamburger/posisi tutup -->
+            <div class="flex justify-end lg:hidden">
+                <svg class="cursor-pointer w-8 sm:mr-5 mr-2 mt-1 fill-current text-secondary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+            </div>
+
+
+            <!-- end sidebar hamburger/posisi tutup -->
+
+            <!-- start sidebar posisi buka -->
+
+            <div class="absolute lg:left-0 -left-72 duration-1000 easy-out w-full">
+                <div class="text-secondary md:text-xl font-bold lg:mt-3 -mt-2 mb-3 text-center">DOKUMENTASI</div>
+                <div class="flex justify-center mt-2 mb-5 h-9 relative w-full">
+                    <input type="text" name="search" class="input w-5/6 pl-2 pr-8 border-2 rounded-lg border-gray-400 md:text-base outline-none text-gray-400" spellcheck="false" id="search" placeholder="Pencarian">
+                    <div class="relative right-8 flex items-center text-gray-500">
+                        <svg class="absolute text-gray-500 hover:text-gray-600 w-7 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                </div>
+
+                <div class="flex justify-center items-center text-secondary bg-primaryHover md:text-xl font-bold py-3">
+                    <div class="flex items-center">
+                        <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0)">
+                                <path d="M0.778756 9.95506C0.626631 9.95354 0.478292 9.90744 0.352104 9.82246C0.225917 9.73749 0.127423 9.61737 0.068816 9.47698C0.0102093 9.33658 -0.00593613 9.18209 0.0223786 9.03261C0.0506934 8.88314 0.122225 8.74525 0.228117 8.63602L8.6277 0.236433C8.77022 0.0897825 8.96457 0.00489081 9.16901 7.13166e-09C9.37535 -2.78609e-05 9.57332 0.081619 9.71965 0.2271L18.1192 8.62669C18.2653 8.77272 18.3473 8.9708 18.3473 9.17733C18.3473 9.38385 18.2653 9.58193 18.1192 9.72797C17.9732 9.874 17.7751 9.95605 17.5686 9.95605C17.3621 9.95605 17.164 9.874 17.018 9.72797L9.16901 1.86657L1.3294 9.73419C1.18225 9.87768 0.984278 9.95709 0.778756 9.95506Z" fill="#FFAA00" />
+                                <path d="M12.9261 18H5.42869C4.56534 17.9967 3.73847 17.6515 3.12916 17.0398C2.51984 16.4282 2.17773 15.6 2.17773 14.7366V7.02145C2.17773 6.81518 2.25967 6.61736 2.40553 6.47151C2.55138 6.32565 2.7492 6.24371 2.95547 6.24371C3.16174 6.24371 3.35956 6.32565 3.50542 6.47151C3.65127 6.61736 3.73321 6.81518 3.73321 7.02145V14.7366C3.73321 15.1896 3.91315 15.624 4.23345 15.9443C4.39204 16.1029 4.58032 16.2287 4.78754 16.3145C4.99475 16.4004 5.21684 16.4445 5.44113 16.4445H12.9385C13.3872 16.438 13.8152 16.2552 14.1302 15.9356C14.4451 15.616 14.6216 15.1853 14.6216 14.7366V7.02145C14.6216 6.81518 14.7035 6.61736 14.8494 6.47151C14.9952 6.32565 15.193 6.24371 15.3993 6.24371C15.6056 6.24371 15.8034 6.32565 15.9493 6.47151C16.0951 6.61736 16.177 6.81518 16.177 7.02145V14.7366C16.1771 15.6 15.8349 16.4282 15.2256 17.0398C14.6163 17.6515 13.7894 17.9967 12.9261 18Z" fill="#FFAA00" />
+                                <path d="M11.3553 15.2002H6.99991C6.89766 15.2006 6.79635 15.1807 6.7018 15.1418C6.60725 15.1029 6.52135 15.0456 6.44905 14.9733C6.37675 14.901 6.31948 14.8151 6.28054 14.7205C6.2416 14.626 6.22176 14.5247 6.22217 14.4224V12.2914C6.22217 11.5076 6.53355 10.7559 7.08779 10.2016C7.64204 9.64737 8.39376 9.336 9.17758 9.336C9.96141 9.336 10.7131 9.64737 11.2674 10.2016C11.8216 10.7559 12.133 11.5076 12.133 12.2914V14.4224C12.1334 14.5247 12.1136 14.626 12.0746 14.7205C12.0357 14.8151 11.9784 14.901 11.9061 14.9733C11.8338 15.0456 11.7479 15.1029 11.6534 15.1418C11.5588 15.1807 11.4575 15.2006 11.3553 15.2002ZM7.77765 13.6447H10.5775V12.2914C10.5775 11.9201 10.43 11.564 10.1675 11.3015C9.90495 11.039 9.54887 10.8915 9.17758 10.8915C8.8063 10.8915 8.45022 11.039 8.18768 11.3015C7.92515 11.564 7.77765 11.9201 7.77765 12.2914V13.6447Z" fill="#FFAA00" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0">
+                                    <rect width="18.3546" height="18" fill="white" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+
+                        <p class="ml-2">SIA WEB SERVICE</p>
+                    </div>
+                    <!-- <div class="editTutup select-none">
+                        <svg class="lg:w-10 md:w-8 sm:w-7 w-6 fill-current cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                        </svg>
+                    </div> -->
+
+                </div>
+
+                <ul class="mr-4 mt-5" id="menuDok">
+                    <li class="pl-7 rounded-r-lg md:text-xl text-secondary font-semibold mb-1 py-0.5">
+                        <a href="" class="py-1">Meminta Data</a>
+                    </li>
+                    <li class="text-secondary relative mb-1">
+                        <div class="pl-5 rounded-r-lg flex justify-between items-center pr-4 md:text-xl font-semibold">
+                            <span class="py-0.5 px-2 cursor-pointer select-none">Data 1</span>
+                            <img src="/img/icon/drop-down.svg" class="text-red-500 cursor-pointer">
+                        </div>
+                        <ul class="text-white text-white transition-all duration-300 easy-out absolute overflow-hidden open-submenu w-full">
+                            <li class="pl-12 py-0.5 rounded-r-lg md:text-lg">
+                                <a href="" class="py-1">Sub-Data 1.1</a>
+                            </li>
+                            <li class="pl-12 py-0.5 rounded-r-lg md:text-lg">
+                                <a href="" class="py-1">Sub-Data 1.2</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="text-secondary relative transform translate-y-16 duration-300">
+                        <div class="pl-5 rounded-r-lg flex justify-between items-center pr-4 md:text-xl font-semibold">
+                            <span class="py-0.5 px-2 cursor-pointer select-none">Judul</span>
+                            <img src="/img/icon/drop-down.svg" class="text-red-500 cursor-pointer">
+                        </div>
+                        <ul class="text-white text-white transition-all duration-300 easy-out absolute overflow-hidden open-submenu w-full">
+                            <li class="pl-12 py-0.5 rounded-r-lg md:text-lg">
+                                <a href="" class="py-1">Sub-Judul 1.1</a>
+                            </li>
+                            <li class="pl-12 py-0.5 rounded-r-lg md:text-lg">
+                                <a href="" class="py-1">Sub-Judul 1.2</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!-- end sidebar posisi buka -->
+
+        </div>
+
+        <!-- content edit di sini -->
+        <div class="w-auto my-7 xl:mx-10 lg:mx-6 mx-6">
+            <?php $this->renderSection('dokumentasi'); ?>
+        </div>
+        <!-- end content edit -->
+    </div>
+
+</div>
+
+</div>
+
+<script>
+    $(document).ready(function() {
+
+
+
+        $('ul li div span').click(function() {
+            ($(this).parent().next().hasClass('close-submenu')) ? $(this).parent().next().removeClass('close-submenu').addClass('open-submenu'): $(this).parent().next().removeClass('open-submenu').addClass('close-submenu')
+            $(this).parent().parent().next().toggleClass('translate-y-16')
+        })
+        $('ul li div img').click(function() {
+            ($(this).parent().next().hasClass('close-submenu')) ? $(this).parent().next().removeClass('close-submenu').addClass('open-submenu'): $(this).parent().next().removeClass('open-submenu').addClass('close-submenu')
+            $(this).parent().parent().next().toggleClass('translate-y-16')
+        })
+
+    });
+</script>
+
+
+
+<?= $this->endSection(); ?>
