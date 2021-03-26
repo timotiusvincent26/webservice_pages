e46b4872d2a6cafa54f7b97b0605b3cccd5e227e
Alwan Hazim <alwanhzm@gmail.com>
2021-03-26 23:53:18 +0700

    revisi laman dokumentasi web service

--- app/Views/webservice/kontenWebservice/dokumentasi/dokumentasi.php
+++ app/Views/webservice/kontenWebservice/dokumentasi/dokumentasi.php
@@ -11,7 +11,7 @@
     <section>
         <div class="text-secondary lg:mx-4 mx-3 ml-10">
             <h1 class="lg:text-xl">Sistem Informasi Alumni</h1>
-            <h2 class="font-bold lg:text-xl">DOKUMENTASI WEB SERVICE</h2>
+            <h2 class="font-bold lg:text-3xl md:text-2xl text-xl">DOKUMENTASI WEB SERVICE</h2>
         </div>
         <hr class="border border-gray-500 transform translate-y-2">
         <div class="lg:mx-4 mx-3 mt-4">
@@ -25,7 +25,7 @@
 
     <section id="memintaData">
         <div class="text-secondary lg:mx-4 mx-3">
-            <h1 class="font-bold lg:text-xl">MEMINTA DATA</h1>
+            <h1 class="font-bold lg:text-3xl md:text-2xl text-xl">Meminta Data</h1>
         </div>
         <hr class="border border-gray-500 transform translate-y-2">
         <div class="lg:mx-4 mx-3 mt-4 mb-8 text-justify">
@@ -41,4 +41,101 @@
     </section>
 </div>
 
+<!-- Data dokumentasi -->
+<div class="sm:ml-12 sm:mr-5 ml-8 mr-2 sm:mt-7 mt-4 transition-none">
+    <div class="text-secondary lg:mx-4 mx-3">
+        <h1 class="font-bold lg:text-3xl md:text-2xl text-xl">Data 1</h1>
+    </div>
+    <hr class="border border-gray-500 transform translate-y-2">
+    <div class="lg:mx-4 mx-3 mt-4 mb-8 text-justify">
+        <p class="">Data 1 adalah Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione modi nesciunt et dicta sunt ab vitae doloribus eveniet, nostrum dolores iusto, laudantium voluptas non maiores quibusdam ipsa, cum voluptates quaerat? </p>
+        <section id="subData1_1">
+            <div class="shadow-2xl w-full lg:my-6 md:my-4 sm:my-2 my-1 rounded-xl px-0 pt-4 py-6 sm:mx-0 mx-6">
+                <div class="text-primary lg:mx-4 mx-3 px-4 py-4">
+                    <h1 class="font-bold text-black lg:text-2xl md:text-xl text-lg">Sub Data 1.1</h1>
+                    <p class="text-black mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione modi nesciunt et dicta sunt ab vitae doloribus eveniet, nostrum dolores iusto, laudantium voluptas non maiores quibusdam ipsa, cum voluptates quaerat? </p>
+                    <h1 class="font-bold text-black lg:text-lg md:text-base text-sm mt-4">Judul Tabel</h1>
+                    <div class="md:overflow-x-auto overflow-x-scroll shadow-2xl rounded-3xl">
+                        <!-- mulai tabel -->
+                        <table class="w-full sm:text-sm text-xs rounded-3xl shadow-2xl md:shadow-none font-paragraph">
+                            <!-- start nama kolom tabel Sub Data 1.1 -->
+                            <thead class="bg-gray-100">
+                                <tr>
+                                    <th class="pt-6 pb-6 px-4 rounded-tl-3xl text-black">Field</th>
+                                    <th class="pt-6 pb-6 px-4 rounded-tl-3xl text-black">Type</th>
+                                    <th class="pt-6 pb-6 px-4 rounded-tl-3xl text-black">Description</th>
+                                </tr>
+                            </thead>
+                            <!-- end nama kolom tabel Sub Data 1.1 -->
+
+                            <!-- start isi tabel Sub Data 1.1 -->
+                            <tbody>
+                                <tr class="border-t-2 border-b-2 bg-white">
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">status</td>
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">String</td>
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">Return status, OK if success and Error if any error occured.</td>
+                                </tr>
+                                <tr class="border-t-2 border-b-2 bg-white">
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">page</td>
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">Number</td>
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">Information page</td>
+                                </tr>
+                                <tr class="h-5 formEdit">
+                                    <td colspan="8" class="rounded-b-3xl"></td>
+                                </tr>
+                            </tbody>
+                            <!-- end isi tabel Sub Data 1.1 -->
+                        </table>
+                    </div>
+                    <h1 class="font-bold text-black lg:text-lg md:text-base text-sm mt-4">Judul Source Code</h1>
+
+                    <!-- mulai source code -->
+
+        </section>
+        <section id="subData1_2">
+            <div class="shadow-2xl w-full lg:my-6 md:my-4 sm:my-2 my-1 rounded-xl px-0 pt-4 py-6 sm:mx-0 mx-6">
+                <div class="text-primary lg:mx-4 mx-3 px-4 py-4">
+                    <h1 class="font-bold text-black lg:text-2xl md:text-xl text-lg">Sub Data 1.2</h1>
+                    <p class="text-black mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione modi nesciunt et dicta sunt ab vitae doloribus eveniet, nostrum dolores iusto, laudantium voluptas non maiores quibusdam ipsa, cum voluptates quaerat? </p>
+                    <h1 class="font-bold text-black lg:text-lg md:text-base text-sm mt-4">Judul Tabel</h1>
+                    <div class="md:overflow-x-auto overflow-x-scroll shadow-2xl rounded-3xl">
+                        <!-- mulai tabel -->
+                        <table class="w-full sm:text-sm text-xs rounded-3xl shadow-2xl md:shadow-none font-paragraph">
+                            <!-- start nama kolom tabel Sub Data 1.2 -->
+                            <thead class="bg-gray-100">
+                                <tr>
+                                    <th class="pt-6 pb-6 px-4 rounded-tl-3xl text-black">Field</th>
+                                    <th class="pt-6 pb-6 px-4 rounded-tl-3xl text-black">Type</th>
+                                    <th class="pt-6 pb-6 px-4 rounded-tl-3xl text-black">Description</th>
+                                </tr>
+                            </thead>
+                            <!-- end nama kolom tabel Sub Data 1.2 -->
+
+                            <!-- start isi tabel Sub Data 1.2 -->
+                            <tbody>
+                                <tr class="border-t-2 border-b-2 bg-white">
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">status</td>
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">String</td>
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">Return status, OK if success and Error if any error occured.</td>
+                                </tr>
+                                <tr class="border-t-2 border-b-2 bg-white">
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">page</td>
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">Number</td>
+                                    <td class="pt-2 pb-2 px-4 rounded-tl-3xl text-black">Information page</td>
+                                </tr>
+                                <tr class="h-5 formEdit">
+                                    <td colspan="8" class="rounded-b-3xl"></td>
+                                </tr>
+                            </tbody>
+                            <!-- end isi tabel Sub Data 1.2 -->
+                        </table>
+                    </div>
+                    <h1 class="font-bold text-black lg:text-lg md:text-base text-sm mt-4">Judul Source Code</h1>
+
+                    <!-- mulai source code -->
+
+        </section>
+    </div>
+
+</div>
 <?= $this->endSection(); ?>
--- app/Views/webservice/kontenWebservice/halamanUtama/utama.php
+++ app/Views/webservice/kontenWebservice/halamanUtama/utama.php
@@ -10,7 +10,7 @@ echo $this->section('content');
 
 <div class="flex sm:static relative xl:mx-28 lg:mx-20 md:mx-12 sm:mx-7 mx-6" id="utama">
     <div class="lg:mr-10 sm:mr-6 mr-0 md:w-8/12 w-full flex flex-col justify-center relative" data-aos="fade-right">
-        <h1 class="lg:text-4xl md:text-3xl sm:text-2xl text-xl font-heading text-secondary font-bold flex justify-center text-center">WEBSERVICE SISTEM INFORMASI ALUMNI</h1>
+        <h1 class="lg:text-4xl md:text-3xl sm:text-2xl text-xl font-heading text-secondary font-bold flex justify-start text-center">WEBSERVICE SISTEM INFORMASI ALUMNI</h1>
         <p class="sm:mt-8 mt-4 text-justify md:text-base text-sm">API Alumni AIS/STIS/Polstat STIS merupakan sebuah layanan yang menyediakan informasi data alumni AIS/STIS/Polstat STIS. Dengan adanya API Alumni AIS/STIS/Polstat STIS, maka para developer dapat mengembangkan aplikasi mobile atau website menggunakan API yang tersedia.</p>
     </div>
     <div class="sm:flex sm:flex-col sm:justify-center hidden xl:w-4/12 sm:w-1/2 relative" data-aos="fade-left">
